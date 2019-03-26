c1176ad6240548c241a0f44ac4dfe11b4301eba6
Yoav Ilan <ilan.yoav@utexas.edu>
2019-03-26 16:49:35 -0500

    working to make relationships better with corenlp, using for training

--- rel_extraction.py
+++ rel_extraction.py
@@ -1,8 +1,26 @@
-import os
+# import os
+#
+# os.chdir(os.getcwd() + '/Stanford_OpenIE_Python')
+# pwd = os.getcwd() + '/corpus'
+#
+# for f in os.listdir(pwd):
+#     print('Processing', f+ '...')
+#     os.system('./process_large_corpus.sh corpus/' + f + ' relations/' +f)
 
-os.chdir(os.getcwd() + '/Stanford_OpenIE_Python')
-pwd = os.getcwd() + '/corpus'
+from stanfordcorenlp import StanfordCoreNLP
 
-for f in os.listdir(pwd):
-    print('Processing', f+ '...')
-    os.system('./process_large_corpus.sh corpus/' + f + ' relations/' +f)
+nlp = StanfordCoreNLP('http://localhost', port=9000)
+
+sentence = "Petr Čech (; born 20 May 1982) is a Czech professional footballer who plays as a goalkeeper for club Arsenal."
+pos = nlp.pos_tag(sentence)
+ner = nlp.ner(sentence)
+with open('stanford-corenlp-full-2018-10-05/training.corp', 'w') as f:
+    for i in range(len(pos)):
+        sent = '0\t' + ner[i][1] + '\t' + str(i) + '\tO\t' + pos[i][1] + '\t' + pos[i][0] + '\tO\tO\tO\n'
+        f.write(sent)
+
+
+# pros = {'annotators': 'ner,relation', 'outputFormat': 'text'}
+# print(nlp.annotate(sentence, properties=pros))
+
+nlp.close() # Do not forget to close! The backend server will consume a lot memery.
