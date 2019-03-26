b760b517833bd75567591b56a7fd69317bd097be
Yoav Ilan <ilan.yoav@utexas.edu>
2019-03-26 16:48:46 -0500

    neo4j support added

---
+++ neo4j.py
@@ -0,0 +1,21 @@
+from py2neo import Graph, Node, Relationship
+import os
+
+graph = Graph("bolt://localhost:7687", auth=("neo4j", "NLP2"))
+
+def add_relationship(entity1, entity2, relation):
+    a = Node("Ent", name=entity1)
+    b = Node("Ent", name=entity2)
+    rel = Relationship.type(relation)
+    graph.merge(rel(a,b), "Ent", "name")
+
+pwd = os.getcwd()# + '/Stanford_OpenIE_Python/relations'
+
+for f in os.listdir(pwd):
+    if(f == "test.txt"):
+        print('Processing', f+ '...')
+        # with open('Stanford_OpenIE_Python/relations/'+f) as relations:
+        with open(f) as relations:
+            for relation in relations:
+                triple = relation.split(' | ')
+                add_relationship(triple[0], triple[2], triple[1])
